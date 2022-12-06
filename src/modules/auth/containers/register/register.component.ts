ff779ed2c94f33952023982e654f0ec63240de74
hayder.barrak@esprit.tn <rafahayder1310>
2022-12-06 14:06:37 +0100

    Project v 0.1 Register Page

--- src/modules/auth/containers/register/register.component.ts
+++ src/modules/auth/containers/register/register.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '@modules/auth/services';
 
 @Component({
     selector: 'sb-register',
@@ -7,6 +9,32 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
     styleUrls: ['register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-    constructor() {}
+    form: any = {
+        username: null,
+        email: null,
+        password: null
+    };
+    isSuccessful = false;
+    isSignUpFailed = false;
+    errorMessage = '';
+    constructor(private authService: AuthService,
+                private route: Router) {}
     ngOnInit() {}
+
+    onSubmit(){
+        const { username, email, password } = this.form;
+
+        this.authService.register(username, email, password).subscribe(
+            data => {
+                console.log(data);
+                this.isSuccessful = true;
+                this.isSignUpFailed = false;
+                this.route.navigate(["/auth/login"]);
+            },
+            err => {
+                this.errorMessage = err.error.message;
+                this.isSignUpFailed = true;
+            }
+        );
+    }
 }
